1aec0e32112f6f286d4f47db5928ebb004f04e9d
Janek Mann <janek.mann@knaufenergy.com>
2022-08-28 14:13:54 +0100

    alternative path

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cd /workspace/
-git clone https://github.com/janekm/stable-diffusion.git
-cd stable-diffusion
+git clone https://github.com/janekm/stable-diffusion.git sd
+cd sd
 python3 scripts/preload_models.py
 cp /weights/sd.ckpt models/ldm/stable-diffusion-v1/model.ckpt
