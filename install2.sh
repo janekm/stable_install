e5909f0998ae51810909aab8b534f0d8975b1171
Janek Mann <janek.mann@knaufenergy.com>
2022-09-08 08:56:13 +0100

    temporarily stop pod stopping

--- install2.sh
+++ install2.sh
@@ -60,4 +60,5 @@ pip install realesrgan
 wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth -P experiments/pretrained_models
 cd /workspace/sd/
 python scripts/dream.py
+sleep infinity
 runpodctl stop pod $RUNPOD_POD_ID
